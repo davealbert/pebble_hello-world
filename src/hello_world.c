2ce9ce62f9c12e3edcee83bc9c12a783407b5f99
Dave Albert <dave.albert@gmail.com>
2015-10-08 05:21:23 +0100

    second text and image

--- src/hello_world.c
+++ src/hello_world.c
@@ -2,19 +2,27 @@
 
 Window *window;
 TextLayer *text_layer;
+TextLayer *text_layer2;
 
 void handle_init(void) {
 	// Create a window and text layer
 	window = window_create();
-	text_layer = text_layer_create(GRect(0, 0, 144, 154));
+	text_layer = text_layer_create(GRect(0, 0, 144, 28));
+	text_layer2 = text_layer_create(GRect(0, 55, 144, 22));
 	
 	// Set the text, font, and text alignment
 	text_layer_set_text(text_layer, "Hi, I'm a Pebble!");
 	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
 	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
 	
+	// Set the text, font, and text alignment
+	text_layer_set_text(text_layer2, "By Dave");
+	text_layer_set_font(text_layer2, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+	text_layer_set_text_alignment(text_layer2, GTextAlignmentCenter);
+	
 	// Add the text layer to the window
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer2));
 
 	// Push the window
 	window_stack_push(window, true);
@@ -26,6 +34,7 @@ void handle_init(void) {
 void handle_deinit(void) {
 	// Destroy the text layer
 	text_layer_destroy(text_layer);
+	text_layer_destroy(text_layer2);
 	
 	// Destroy the window
 	window_destroy(window);
